26b6a7585e31009459e94a98048a95be997043ed
WNDH-VD3m <77713690+WNDH-VD3m@users.noreply.github.com>
2021-02-05 17:59:34 +0800

    用.width 设置列的宽度时注意空格
    如.width 10, 20, 10, 30 数字与数字中间有空格

---
+++ demo3.sql
@@ -0,0 +1,11 @@
+sqlite> .width 10, 20, 10, 10, 10
+sqlite> SELECT * FROM COMPANY;
+ID          NAME                  AGE         ADDRESS        SALARY
+----------  --------------------  ----------  -------------  ----------
+1           TOM                   20          XINGHE STREET  42000.0
+2           MIKE                  23          PINGKS STREET  12000.0
+3           DHE                   13          ADAGKS STREET  1300.0
+4           Mark                  25          Rich-Mond      65000.0
+5           David                 27          Texas          85000.0
+6           Kim                   22          South-Hall     45000.0
+7           James                 24          Houston        10000.0   
